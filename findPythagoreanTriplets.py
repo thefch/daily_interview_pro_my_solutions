645f984a4f0d209943ffa794574407fad74d3e17
thefch <theofanis.ch@hotmail.com>
2020-06-20 19:23:09 +0300

    pythagorean triplets

--- findPythagoreanTriplets.py
+++ findPythagoreanTriplets.py
@@ -1,16 +1,21 @@
+import math
 def findPythagoreanTriplets(nums):
+    # list with the squared nums
     list = [i**2 for i in nums]
+    
     for i in range(0,len(list),1):
-        print(list[i])
         for j in range(0,len(list),1):
+        
+            #triplet total
             total = list[j] + list[i]
             
+            # it must not check its self
+            # if the totla is in the list, triplet exists
             if (total) in list and (i is not j):
-                print('=>',nums[j],'**2 +',nums[i],'**2 =',total,'**2')
+                
+                print('=> {}**2 + {}**2 = {}**2'.format(nums[j],nums[i],int(math.sqrt(total))))
+                print('=> {} + {} = {}'.format(list[j],list[i],total))
                 return True
-                
-                
-               
     return False
 
 print (findPythagoreanTriplets([3, 12, 5, 13]))
